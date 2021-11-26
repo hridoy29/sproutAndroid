61353a76e1cc94d3360dc22672fe337d23d1bc2f
jubair hridoy <jubair_29@outlook.com>
2021-11-26 17:12:16 +0600

    000

--- app/src/main/java/com/example/pantomime/RetrofitClient.java
+++ app/src/main/java/com/example/pantomime/RetrofitClient.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
-    private static final String BASE_URL ="http://api.pantomimeltd.com/api/"; //"https://jsonplaceholder.typicode.com/";
+    private static final String BASE_URL ="https://api.pantomimeltd.com/api/"; //"https://jsonplaceholder.typicode.com/";
     private static RetrofitClient mInstance;
     private final Retrofit retrofit;
 
